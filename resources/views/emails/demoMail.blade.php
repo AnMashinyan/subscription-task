a0876635baea989cfa3d79c43f995cfee700ec84
AnMashinyan <ani.mashinayan00@gmail.com>
2023-03-10 11:19:05 +0400

    That's the first commitment!

---
+++ resources/views/emails/demoMail.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ItsolutionStuff.com</title>
+</head>
+<body>
+<p>New post was created</p>
+<h1> {{$post->title}}</h1>
+
+<p>{{$post->description}}</p>
+<p>Thank you</p>
+</body>
+</html>
+
+
+
